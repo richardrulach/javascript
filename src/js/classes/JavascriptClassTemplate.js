a9b20974f7ff4a90c1b5836faafe6c6ea47cc44d
waystoskinacat <git@waystoskinacat.com>
2014-05-17 16:21:06 +0200

    Created Javascript Class Template

---
+++ src/js/classes/JavascriptClassTemplate.js
@@ -0,0 +1,29 @@
+/**
+  * Class: [CLASSNAME]
+  * File: [FILENAME].js 
+  *
+  * Definition:
+  *
+  *
+ **/
+ var [CLASSNAME] = {
+
+   	/* CLASS CONSTANTS */
+
+
+    /* CLASS PROPERTIES */
+
+
+    /* INITIALISERS */
+
+
+	/* EXTERNAL FUNCTIONS */
+
+
+	/* INTERNAL FUNCTIONS */
+
+
+ };
+ /**
+  * END CLASS DEFINITION
+ **/

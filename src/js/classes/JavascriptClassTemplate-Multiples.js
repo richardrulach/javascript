97897b68771aa00601740e8817acf63fc0282074
waystoskinacat <git@waystoskinacat.com>
2014-05-19 16:04:12 +0200

    Redeveloped Class Templates

---
+++ src/js/classes/JavascriptClassTemplate-Multiples.js
@@ -0,0 +1,33 @@
+/**
+  * Class: [CLASSNAME]
+  * File: [FILENAME].js 
+  *
+  * Definition:
+  *
+  *
+ **/
+function [CLASSNAME](){
+
+    /* CLASS CONSTANTS */
+    this.CONSTANT = 0;
+
+    /* CLASS PROPERTIES */
+    this.property = 0;
+
+
+    /* EXTERNAL FUNCTIONS */
+    this.ExternalFunction = function(){
+
+    };
+
+
+    /* INTERNAL FUNCTIONS */
+    this.InternalFunction = function(){
+      
+    };
+
+
+};
+ /**
+  * END CLASS DEFINITION
+ **/

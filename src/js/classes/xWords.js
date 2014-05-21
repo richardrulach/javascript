bc9e86cfd7438066f4bf8184f0b87dc23eb5d75d
waystoskinacat <git@waystoskinacat.com>
2014-05-21 19:39:30 +0200

    stable version

--- src/js/classes/xWords.js
+++ src/js/classes/xWords.js
@@ -1,10 +1,17 @@
-/* xWords.js */
-/* Dependencies JQuery */
+/**
+  * Class: xWords
+  * File: xWords.js 
+  *
+  * Definition:
+  *		Creates and manages a crossword.
+  *
+  * Dependencies:
+  * 	jQuery
+ **/
 var xWords = {
-	/* CONSTANTS */
+   	/* CLASS CONSTANTS */
 
-	
-	/* VARIABLES */
+    /* CLASS PROPERTIES */
     _self:this,
 	Grid:new Array(),
 	Words:new Array(),
@@ -12,13 +19,14 @@ var xWords = {
 
 	/* PUBLIC METHODS */
 	// resets the grid and word list
-	Clear: function(){
+	Reset: function(){
 		this.Grid = new Array();
 		this.Words = new Array();
 	},
 
 
 	Create: function(height, width, arrayOfWords){
+		this.Reset();
         var newGrid = new Array(width);
         for (var i = 0; i < newGrid.length; i++){
             newGrid[i] = new Array(height);
@@ -31,7 +39,13 @@ var xWords = {
         }
 
 		this.Grid = newGrid;
+
 		console.log(this.SortByLength(arrayOfWords));
+
+		for (var x=0; x < arrayOfWords.length; x++){
+			 this.AddWord(arrayOfWords[x]);
+		}
+		// console.log(this.Grid);
 		return this.Grid;
 	},
 
@@ -44,7 +58,136 @@ var xWords = {
 
 
 	AddWord: function(newWord){
+		var positions = this.GetPositions(newWord);
 
+		if (positions.length > 0){
+
+			var choice = -1;
+
+			// CHOOSE THE FIRST ONE WITH A CROSSING POINT
+			// IF IT IS THERE
+			for (var iCount = 0; iCount < positions.length; iCount++){
+				if (positions[iCount].crossingPoint == 1){
+					choice = iCount;
+				}
+			}
+
+			if (choice == -1){
+				choice = Math.floor(
+					(Math.random() * positions.length));
+			}
+
+			// WRITE THE WORD INTO THE ARRAY
+			var newPos = positions[choice];
+			console.log(newPos);
+
+			// LOOP THROUGH THE WORD PLACING IT IN THE GRID
+			for (var count = 0; count < newWord.length; count++){
+				if (newPos.direction == 1){
+					this.Grid[newPos.x + count][newPos.y] = 
+						newWord.charAt(count);
+				} else if (newPos.direction == -1){
+					this.Grid[newPos.x][newPos.y + count] = 
+						newWord.charAt(count);
+				}
+			}
+		}
+	},
+
+	GetPositions: function(newWord){
+		var positionArray = new Array();
+
+		for (var x = 0; x < this.Grid.length; x++){
+			for (var y = 0; y < this.Grid[0].length; y++){
+				
+				var newPos = this.TestPosition(newWord,x,y,1);
+				if (newPos !== undefined) positionArray.push(newPos);
+
+				newPos = this.TestPosition(newWord,x,y,-1);
+				if (newPos !== undefined) positionArray.push(newPos);
+
+				// if (this.TestPosition(newWord,x,y,1)){
+				// 	positionArray.push(new Position(x,y,1));
+				// }
+				// if (this.TestPosition(newWord,x,y,-1)){
+				// 	positionArray.push(new Position(x,y,-1));
+				// }
+			}
+		}
+		console.log(newWord + ' pos' + positionArray.length);
+		return positionArray;
+	},
+
+	TestPosition: function(newWord,x,y,direction){
+
+		var crossingPoint = 0;
+
+		if (direction == 1){
+
+			if (x + newWord.length > this.Grid.length)
+				return;
+
+			for (var count = 0; count < newWord.length; count++){
+				if ((this.Grid[x + count][y].length > 0)&&
+					(this.Grid[x + count][y] != 
+						newWord.charAt(count))){
+					return;
+				} else if (this.Grid[x + count][y] == 
+						newWord.charAt(count).toString()){
+					crossingPoint = 1;
+					// console.log("newWord:" + newWord );
+					// console.log("x:" + x );
+					// console.log("y:" + y );
+					// console.log("direction:" + direction );
+				} else if (this.SidesHaveChars(x+count,y,direction)){
+					return;
+				} 
+			}
+
+		} else if (direction == -1){
+
+			if (y + newWord.length > this.Grid[0].length)
+				return;
+
+			for (var count = 0; count < newWord.length; count++){
+				if ((this.Grid[x][y + count].length > 0)&&
+					(this.Grid[x][y + count] != 
+						newWord.charAt(count))) 
+					return;
+				else if (this.Grid[x][y + count] == 
+						newWord.charAt(count).toString()){
+					crossingPoint = 1;
+				} else if (this.SidesHaveChars(x,y+count,direction)){
+					return;
+				} 
+			}
+
+		}
+
+		// If no problems - return the details!!!
+		return new Position(x,y,direction,crossingPoint);
+	},
+
+	SidesHaveChars: function(x,y,direction){
+		var bHasChars = false;
+
+		if (direction == 1){
+			if (y-1 > 0){
+				if (this.Grid[x][y-1].length > 0) return true;
+			}
+			if (y+1 < this.Grid[0].length){
+				if (this.Grid[x][y+1].length > 0) return true;
+			}
+		} else if (direction == -1){
+			if (x-1 > 0){
+				if (this.Grid[x-1][y].length > 0) return true;
+			}
+			if (x+1 < this.Grid.length){
+				if (this.Grid[x+1][y].length > 0) return true;
+			}
+		}
+
+		return bHasChars;
 	},
 
 	GetWords: function(){
@@ -72,3 +215,16 @@ var xWords = {
 
 
 };
+ /**
+  * END CLASS DEFINITION
+  **/
+
+// Direction: 
+//  1 indicates horizontal
+// -1 indicates vertical
+function Position(x,y,direction,crossingPoint){
+	this.x = x;
+	this.y = y;
+	this.direction = direction;
+	this.crossingPoint = crossingPoint;
+}
